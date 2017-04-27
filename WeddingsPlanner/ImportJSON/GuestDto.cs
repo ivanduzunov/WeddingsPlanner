5ccce2982eb722bf9ef987023e705837c9f0f994
ivanduzunov <ivan.d.uzunov@gmail.com>
2017-04-28 00:34:50 +0300

    ss

---
+++ WeddingsPlanner/ImportJSON/GuestDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeddingsPlanner.ImportJSON
+{
+    public class GuestDto
+    {
+        public string Name { get; set; }
+        public string RSVP { get; set; }
+        public string Family { get; set; }
+    }
+}
---
+++ WeddingsPlanner/ImportJSON/WeddingDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using WeddingsPlanner.Models;
+
+namespace WeddingsPlanner.ImportJSON
+{
+    public class WeddingDto
+    {
+        public WeddingDto()
+        {
+            this.Guests = new HashSet<GuestDto>();
+        }
+        public string Bride { get; set; }
+        public string Bridegroom { get; set; }
+        public DateTime Date { get; set; }
+        public string Agency { get; set; }
+        public virtual ICollection<GuestDto> Guests { get; set; }
+    }
+}
+
